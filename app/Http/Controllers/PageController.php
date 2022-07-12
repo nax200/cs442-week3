b8e4194a60e335c5343bdb73d988aa48424a6fa9
nax200 <thanabodee.ka@ku.th>
2022-07-12 16:33:22 +0700

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        return view('pages.index');
+    }
+    public function show($id){ //associative array
+        return view('pages.show', [
+            'id' => $id,
+            'name' => 'nax'
+        ]);
+    }
+}
---
+++ resources/views/pages/show.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>Pages {{$id}}</h1>
+    <h2>By {{$name}}</h2>
+@endsection
