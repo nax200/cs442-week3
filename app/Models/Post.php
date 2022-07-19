9c5459abf90d90b92e0907de30837c57c5b2e52f
nax200 <thanabodee.ka@ku.th>
2022-07-19 16:09:24 +0700

    second

---
+++ app/Models/Post.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory; //trait
+
+    public function scopeAdvertise($query)
+    {
+        return $query ->where('like_count','<',1000)
+                      ->where('view_count','>',70000);
+    }
+    public function scopePopular($query,$like_count,$view_count)
+    {
+        return $query ->where('like_count','>=',$like_count)
+                      ->where('view_count','>=',$view_count);
+    }
+    public function scopeFilterTitle($query,$search)
+    {
+        return $query ->where('title','LIKE',"%{$search}%"); // %wild card
+    }
+}
+
